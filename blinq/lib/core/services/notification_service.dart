959d5ab8799039dff68a6505d9aee3feb7320553
luisg0c <146095936+luisg0c@users.noreply.github.com>
2025-05-24 15:41:38 -0300

    fix

--- blinq/lib/core/services/notification_service.dart
+++ blinq/lib/core/services/notification_service.dart
@@ -1,5 +1,3 @@
-// lib/core/services/notification_service.dart
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
