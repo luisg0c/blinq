f872a3dab352064e1e8c10a0363cffedeb0deb3b
luizhsrdev <luizhsrdev@gmail.com>
2025-05-25 23:59:25 -0300

    Remoção de duplicata.

--- blinq/lib/presentation/bindings/splash_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class SplashBinding extends Bindings {
-  @override
-  void dependencies() {
-    print('🔧 Inicializando SplashBinding...');
-    print('✅ SplashBinding inicializado');
-  }
-}
